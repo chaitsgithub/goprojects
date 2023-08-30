49c8736c818aff391bb435a861ec3abab82d96b4
chaitsgithub <vuppuluri@gmail.com>
2023-08-30 21:01:40 +0530

    Removing Job Stats

--- job-stats/src/services/jobstats/jobstats.ts
+++
@@ -1,9 +0,0 @@
-export interface JobStats {
-    jobName?: string;
-    runDate?: string;
-    jobID?: number;
-    startTime?: string;
-    endTime?: string;
-    runTime?: string;
-    status?: string;
-}
